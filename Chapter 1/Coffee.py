4f9aa443a6ef40a73cbcf207000e49be4dcf9efe
Shane <69217993+CreatingCoder@users.noreply.github.com>
2022-05-23 21:03:32 -0500

    Create Coffee.py

---
+++ Chapter 1/Coffee.py
@@ -0,0 +1,8 @@
+class Coffee:
+    def __init__(self, roast, sweetner):
+        self.roast= roast
+        self.sweetner = sweetner
+    
+
+myCoffee = Coffee("Dark", "Sweet n Low")
+print(myCoffee.roast + " roast, with " + myCoffee.sweetner + " sweetner")
