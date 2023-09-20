c5af0b97ce25763978321116b63f9b9c28ce2501
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-09-20 22:34:07 +0000

    Update Coffee.py

--- Chapter 1/Coffee.py
+++ Chapter 1/Coffee.py
@@ -3,6 +3,5 @@ class Coffee:
         self.roast= roast
         self.sweetner = sweetner
     
-
 myCoffee = Coffee("Dark", "Sweet n Low")
 print(myCoffee.roast + " roast, with " + myCoffee.sweetner + " sweetner")
